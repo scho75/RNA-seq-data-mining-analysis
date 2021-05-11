0f61b6289d4bdca506c18c6a540a2841e3380c2f
scho75 <44254195+scho75@users.noreply.github.com>
2021-05-11 14:08:06 +1000

    Update violin_plotter.R

--- violin_plotter.R
+++ violin_plotter.R
@@ -5,27 +5,24 @@ violin_plotter <- function(df, dds, desired_parameter, save = 'F', savedirectory
   # edit inputted colnames
   desired_parameter <- gsub('-','.',desired_parameter)
   
-  if (desired_parameter %in% c('viral_load','age')){
-    df <- df[which(df$immune_influx != 'negative'),] #omit SARS-CoV-2 negative patients
+  if (desired_parameter %in% c('age')){
     df <- df[which(!is.na(df[,desired_parameter])),] #drop NA records
     y <- desired_parameter
-    pval <- round(wilcox.test(df[,desired_parameter]~df[,"immune_influx"])$p.value, digits = 5)
-    fold_change <- round(mean(df[which(df$immune_influx == 'high'),desired_parameter]) / mean(df[which(df$immune_influx == 'low'),desired_parameter]), digits = 5)
+    pval <- round(wilcox.test(df[,desired_parameter]~df[,"infection_status"])$p.value, digits = 5)
+    fold_change <- round(mean(df[which(df$infection_status == 'positive'),desired_parameter]) / mean(df[which(df$infection_status == 'negative'),desired_parameter]), digits = 5)
     color_choice <- c('#FFD700','#FF0000')
     if(desired_parameter == 'age'){
       y_label <- 'Age (years)'
-    } else if (desired_parameter == 'viral_load'){
-      y_label <- 'N1 CT value'
     }
+    
   } else { #for when desired_parameter is a gene
     df[,desired_parameter] <- log2(df[,desired_parameter] + 1) #log2 transform
     df_ref <- df[which(df$infection_status == 'negative'),] #set reference
-    df <- df[which(df$immune_influx != 'high'),] #omit SARS-CoV-2 positive high immune influx patients
-    res <- results(dds,contrast=c("immune_influx","low","negative"))
+    res <- results(dds,contrast=c("infection_status","positive",'negative'))
     rownames(res) <- gsub('-','.',rownames(res)) #change res rownames to match
     pval <- formatC(res[rownames(res) == desired_parameter, 'padj'], format = 'e', digits = 2) #format pval
     fold_change <- as.character(round(2^(res[rownames(res) == desired_parameter, 'log2FoldChange']),4))
-    y_label <- paste("Gene expression", "(Z-score log2 normalized count)", sep = '\n')
+    y_label <- paste("Gene expression", "(Z-score normalized count)", sep = '\n')
     color_choice <- c('#16A085','#C0392B')
     
     #z-score transformation
@@ -37,7 +34,7 @@ violin_plotter <- function(df, dds, desired_parameter, save = 'F', savedirectory
   
 
   # Plotting
-  dp <- ggplot(df, aes_string(x="immune_influx", y=y, fill="immune_influx")) + 
+  dp <- ggplot(df, aes_string(x="infection_status", y=y, fill="infection_status")) + 
     geom_violin(trim=FALSE, alpha = 0.8)+
     geom_boxplot(width=0.2, fill="white")+
     labs(title=desired_parameter,x= paste('', '\n','p = ',pval,'\n', 'FC = ',fold_change, sep=""), y = y_label)
@@ -49,7 +46,7 @@ violin_plotter <- function(df, dds, desired_parameter, save = 'F', savedirectory
   if (save == 'F'){
     dp
   } else if (save == 'T'){
-    savedirectory = './' #where you want to save, defaults to current folder
+    savedirectory = savedirectory #where you want to save, defaults to current folder
     fname = paste(desired_parameter,".png",sep="")
     print(paste0("Saving...",fname))
     ggsave(path = savedirectory,
